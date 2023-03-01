13577909f4c4f3878bec8c780a13639da9b7d149
khs <hyeongs2323@gmail.com>
2023-03-01 21:49:29 +0900

    feat(data): dummy data add

--- src/services/data.js
+++ src/services/data.js
@@ -1,5 +1,3 @@
-export const cellKind = ["매매", "전세", "월세"];
-export const priceArr = [0, 50, 100, 150, 200, 250, 300];
 export const cities = [
   "서울",
   "부산",
@@ -19,3 +17,157 @@ export const cities = [
   "경상남도",
   "제주",
 ];
+
+export const rooms = [
+  {
+    id: "1",
+    img: "https://thumb.mt.co.kr/06/2020/09/2020091115075836165_1.jpg/dims/optimize/",
+    contents: "전세 3억",
+    size: "29.82m²",
+    address: "서초구 반포동",
+    info: "역세권 깨끗하고 깔끔한 전세투룸",
+    seller: {
+      brokerActive: true,
+      ownerActive: false,
+    },
+  },
+  {
+    id: "2",
+
+    img: "https://thumb.mt.co.kr/06/2020/09/2020091115075836165_1.jpg/dims/optimize/",
+    contents: "전세 3억",
+    size: "29.82m²",
+    address: "서초구 반포동",
+    info: "역세권 깨끗하고 깔끔한 전세투룸",
+    seller: {
+      brokerActive: false,
+      ownerActive: false,
+    },
+  },
+  {
+    id: "3",
+
+    img: "https://thumb.mt.co.kr/06/2020/09/2020091115075836165_1.jpg/dims/optimize/",
+    contents: "전세 3억",
+    size: "29.82m²",
+    address: "서초구 반포동",
+    info: "역세권 깨끗하고 깔끔한 전세투룸",
+    seller: {
+      brokerActive: true,
+      ownerActive: true,
+    },
+  },
+  {
+    id: "4",
+
+    img: "https://thumb.mt.co.kr/06/2020/09/2020091115075836165_1.jpg/dims/optimize/",
+    contents: "전세 3억",
+    size: "29.82m²",
+    address: "서초구 반포동",
+    info: "역세권 깨끗하고 깔끔한 전세투룸",
+    seller: {
+      brokerActive: true,
+      ownerActive: false,
+    },
+  },
+  {
+    id: "5",
+
+    img: "https://thumb.mt.co.kr/06/2020/09/2020091115075836165_1.jpg/dims/optimize/",
+    contents: "전세 3억",
+    size: "29.82m²",
+    address: "서초구 반포동",
+    info: "역세권 깨끗하고 깔끔한 전세투룸",
+    seller: {
+      brokerActive: true,
+      ownerActive: true,
+    },
+  },
+  {
+    id: "6",
+
+    img: "https://thumb.mt.co.kr/06/2020/09/2020091115075836165_1.jpg/dims/optimize/",
+    contents: "전세 3억",
+    size: "29.82m²",
+    address: "서초구 반포동",
+    info: "역세권 깨끗하고 깔끔한 전세투룸",
+    seller: {
+      brokerActive: false,
+      ownerActive: false,
+    },
+  },
+  {
+    id: "7",
+
+    img: "https://thumb.mt.co.kr/06/2020/09/2020091115075836165_1.jpg/dims/optimize/",
+    contents: "전세 3억",
+    size: "29.82m²",
+    address: "서초구 반포동",
+    info: "역세권 깨끗하고 깔끔한 전세투룸",
+    seller: {
+      brokerActive: true,
+      ownerActive: false,
+    },
+  },
+  {
+    id: "8",
+
+    img: "https://thumb.mt.co.kr/06/2020/09/2020091115075836165_1.jpg/dims/optimize/",
+    contents: "전세 3억",
+    size: "29.82m²",
+    address: "서초구 반포동",
+    info: "역세권 깨끗하고 깔끔한 전세투룸",
+    seller: {
+      brokerActive: true,
+      ownerActive: false,
+    },
+  },
+  {
+    id: "9",
+
+    img: "https://thumb.mt.co.kr/06/2020/09/2020091115075836165_1.jpg/dims/optimize/",
+    contents: "전세 3억",
+    size: "29.82m²",
+    address: "서초구 반포동",
+    info: "역세권 깨끗하고 깔끔한 전세투룸",
+    seller: {
+      brokerActive: true,
+      ownerActive: false,
+    },
+  },
+  {
+    id: "10",
+
+    img: "https://thumb.mt.co.kr/06/2020/09/2020091115075836165_1.jpg/dims/optimize/",
+    contents: "전세 3억",
+    size: "29.82m²",
+    address: "서초구 반포동",
+    info: "역세권 깨끗하고 깔끔한 전세투룸",
+    seller: {
+      brokerActive: true,
+      ownerActive: false,
+    },
+  },
+  {
+    id: "11",
+
+    img: "https://thumb.mt.co.kr/06/2020/09/2020091115075836165_1.jpg/dims/optimize/",
+    contents: "전세 3억",
+    size: "29.82m²",
+    address: "서초구 반포동",
+    info: "역세권 깨끗하고 깔끔한 전세투룸",
+    seller: {
+      brokerActive: true,
+      ownerActive: false,
+    },
+  },
+];
+
+export const options = {
+  cellKind: ["매매", "전세", "월세"],
+  isStationArea: ["불필요", "필요"],
+  room_counts: ["1개", "2개", "3개", "4개 이상"],
+  toilet_counts: ["1개", "2개", "3개", "4개 이상"],
+  py: ["10", "20", "30", "40", "50", "50+"],
+  priceArr: ["0", "50", "100", "150", "200", "250", "300", "300+"],
+};
