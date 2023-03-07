06341d4fb0265985787e2bf04b64f9a4838ecace
khs <hyeongs2323@gmail.com>
2023-03-07 14:17:12 +0900

    feat(menu): filteMenu add

---
+++ src/components/Button/FilterMenu.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import {
+  Box,
+  Button,
+  VStack,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
+import { IoIosArrowDown } from "react-icons/io";
+
+import { options } from "../../services/data";
+
+import DataRadioCard from "../Radio/RadioCard";
+
+function FilterMenu({ names, onUpdate }) {
+  return (
+    <Menu>
+      <MenuButton
+        as={Button}
+        colorScheme="facebook"
+        p="15px"
+        rightIcon={<IoIosArrowDown />}
+      >
+        {names.kor}
+      </MenuButton>
+      <MenuList>
+        <MenuItem>추가필터</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+}
+export default FilterMenu;
