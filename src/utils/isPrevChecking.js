81351a740595e24b9a103aa0593d461ba9e66de8
khs <hyeongs2323@gmail.com>
2023-03-08 15:22:57 +0900

    refactor: isLoacl -> isVal func name change

--- src/utils/isPrevChecking.js
+++ src/utils/isPrevChecking.js
@@ -1,11 +1,11 @@
-import { isLocal } from "../services/local";
+import { isVal } from "../services/local";
 
 export const isPrevChecking = (list, name) => {
   const index = list.indexOf(name) - 1;
 
   const val = localStorage.getItem(list[index]);
 
-  if (isLocal(val) || index < 0) {
+  if (isVal(val) || index < 0) {
     return true;
   } else {
     return false;
