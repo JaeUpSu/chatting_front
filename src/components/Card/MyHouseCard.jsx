2c8d8b0b00477c49d6a0a38368a361f731f2d31a
khs <hyeongs2323@gmail.com>
2023-03-23 16:04:27 +0900

    feat: Navigate -> Link change

--- src/components/Card/MyHouseCard.jsx
+++ src/components/Card/MyHouseCard.jsx
@@ -8,7 +8,7 @@ import {
   HStack,
 } from "@chakra-ui/react";
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { RoomKindsToFront, SellKindsToFront } from "../../services/data";
 import { getSaleContents } from "../../utils/getSaleContents";
 
@@ -16,7 +16,6 @@ function MyHouseCard({
   thumnail,
   title,
   sell_kind,
-  description,
   room_kind,
   id,
   deposit,
@@ -25,10 +24,6 @@ function MyHouseCard({
   is_sale,
 }) {
   const navigation = useNavigate();
-
-  const onHouseDetail = () => {
-    navigation(`house/${id}`);
-  };
 
   useEffect(() => {
     console.log(title, title.length);
@@ -40,46 +35,47 @@ function MyHouseCard({
       boxShadow="0px"
       _hover={{ backgroundColor: "rgb(140,140,140,0.1)" }}
     >
-      <CardBody
-        display={"flex"}
-        alignItems="center"
-        justifyContent={"center"}
-        onClick={onHouseDetail}
-        cursor="pointer"
-      >
-        <VStack w={"100%"} alignItems="flex-start" spacing={"5"}>
-          <Box
-            backgroundImage={thumnail}
-            backgroundSize="cover"
-            backgroundRepeat="no-repeat"
-            backgroundPosition="center"
-            width="100%"
-            alt="house"
-            borderRadius="lg"
-            css={{
-              aspectRatio: "1 / 1",
-            }}
-          />
-          <Heading fontSize="1.5em" color="blackAlpha.800" w="100%">
-            <HStack justifyContent="space-between" w="100%">
-              <Text>
-                {title?.length > 10 ? title.substring(0, 6) + "..." : title}
+      <Link to={`/houseList/house/${id}`}>
+        <CardBody
+          display={"flex"}
+          alignItems="center"
+          justifyContent={"center"}
+          cursor="pointer"
+        >
+          <VStack w={"100%"} alignItems="flex-start" spacing={"5"}>
+            <Box
+              backgroundImage={thumnail}
+              backgroundSize="cover"
+              backgroundRepeat="no-repeat"
+              backgroundPosition="center"
+              width="100%"
+              alt="house"
+              borderRadius="lg"
+              css={{
+                aspectRatio: "1 / 1",
+              }}
+            />
+            <Heading fontSize="1.5em" color="blackAlpha.800" w="100%">
+              <HStack justifyContent="space-between" w="100%">
+                <Text>
+                  {title?.length > 10 ? title.substring(0, 6) + "..." : title}
+                </Text>
+                <Text fontSize={"lg"} color="red.400" fontWeight={"bold"}>
+                  {is_sale ? "판매중" : "판매 완료"}
+                </Text>
+              </HStack>
+            </Heading>
+            <VStack alignItems={"flex-start"} width="100%">
+              <Text color="blackAlpha.800" fontSize="1.1em" fontWeight="600">
+                {`${RoomKindsToFront[room_kind]} ${SellKindsToFront[sell_kind]}`}
               </Text>
-              <Text fontSize={"lg"} color="red.400" fontWeight={"bold"}>
-                {is_sale ? "판매중" : "판매 완료"}
+              <Text fontSize="1.1em" fontWeight="600">
+                {`${getSaleContents(sell_kind, deposit, monthly_rent, sale)}`}
               </Text>
-            </HStack>
-          </Heading>
-          <VStack alignItems={"flex-start"} width="100%">
-            <Text color="blackAlpha.800" fontSize="1.1em" fontWeight="600">
-              {`${RoomKindsToFront[room_kind]} ${SellKindsToFront[sell_kind]}`}
-            </Text>
-            <Text fontSize="1.1em" fontWeight="600">
-              {`${getSaleContents(sell_kind, deposit, monthly_rent, sale)}`}
-            </Text>
+            </VStack>
           </VStack>
-        </VStack>
-      </CardBody>
+        </CardBody>
+      </Link>
     </Card>
   );
 }
